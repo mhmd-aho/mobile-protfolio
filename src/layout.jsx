df9e69c1150319d96e68a2f46e13d89aedc72de3
mhmdah <mhmdabouhamoud@gmail.com>
2025-12-15 14:55:57 +0200

    fixed router bug

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router";
 import Header from "/src/components/header";
 import Pattern from "/src/components/background";
-import {useState} from "react";
+import {useState,useEffect} from "react";
 export default function Layout() {
-    const [theme,setTheme] = useState("dark")
+    const [theme,setTheme] = useState(()=>localStorage.getItem("theme") || "dark")
+    useEffect(() => {
+        localStorage.setItem("theme",theme);
+    }, [theme]);
     return (
         <div className={theme}>
             <Pattern theme={theme}/>
